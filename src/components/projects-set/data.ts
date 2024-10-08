8bc1cf1ea7a3dcf620b759db4a74c7a1de35c419
Mauro Morales <mauroale2501@gmail.com>
2024-10-08 12:08:43 +0200

    feat: Add skills to skillset

--- src/components/projects-set/data.ts
+++ src/components/projects-set/data.ts
@@ -1,28 +1,28 @@
 export const projectsSet = [
   {
     id: 1,
-    video: "https://www.youtube.com/embed/ojryZ3obYR4?si=1lYJZqsKs0JVsZ8Q",
+    video: "https://www.youtube.com/embed/5RSIMj-CaCk?si=KKYKk4r7aFMJb-C4",
     title: "Project 1",
     description: "Description 1",
     link: "image1.jpg",
   },
   {
     id: 2,
-    video: "https://www.youtube.com/embed/ojryZ3obYR4?si=1lYJZqsKs0JVsZ8Q",
+    video: "https://www.youtube.com/embed/5RSIMj-CaCk?si=KKYKk4r7aFMJb-C4",
     title: "Project 2",
     description: "Description 2",
     link: "image2.jpg",
   },
   {
     id: 3,
-    video: "https://www.youtube.com/embed/ojryZ3obYR4?si=1lYJZqsKs0JVsZ8Q",
+    video: "https://www.youtube.com/embed/5RSIMj-CaCk?si=KKYKk4r7aFMJb-C4",
     title: "Project 3",
     description: "Description 3",
     link: "image3.jpg",
   },
   {
     id: 4,
-    video: "https://www.youtube.com/embed/ojryZ3obYR4?si=1lYJZqsKs0JVsZ8Q",
+    video: "https://www.youtube.com/embed/5RSIMj-CaCk?si=KKYKk4r7aFMJb-C4",
     title: "Project 3",
     description: "Description 3",
     link: "image3.jpg",
--- src/components/skill-set/data.ts
+++ src/components/skill-set/data.ts
@@ -21,34 +21,107 @@ export const skills = [
   },
   {
     id: 5,
+    name: "Maven",
+    icon: "maven",
+  },
+  {
+    id: 6,
     name: "React",
     icon: "react",
   },
-
   {
-    id: 6,
+    id: 7,
     name: "Next.js",
     icon: "nextjs",
   },
-
   {
-    id: 7,
+    id: 8,
     name: "Node.js",
     icon: "nodejs",
   },
   {
-    id: 8,
+    id: 9,
     name: "PostgreSQL",
     icon: "postgresql",
   },
   {
-    id: 9,
+    id: 10,
+    name: "Google Cloud",
+    icon: "gcp",
+  },
+  {
+    id: 11,
     name: "Azure",
     icon: "azure",
   },
   {
-    id: 10,
+    id: 12,
     name: "Docker",
     icon: "docker",
-  }
+  },
+  {
+    id: 13,
+    name: "Cloduinary",
+    icon: "cloudinary",
+  },
+  {
+    id: 14,
+    name: "Git",
+    icon: "git",
+  },
+  {
+    id: 15,
+    name: "GitHub",
+    icon: "github",
+  },
+  {
+    id: 16,
+    name: "Html",
+    icon: "html",
+  },
+  {
+    id: 17,
+    name: "CSS",
+    icon: "css",
+  },
+  {
+    id: 18,
+    name: "Material UI",
+    icon: "materialui",
+  },
+  {
+    id: 19,
+    name: "Jest",
+    icon: "jest",
+  },
+  {
+    id: 20,
+    name: "JUnit",
+    icon: "junit",
+  },
+  {
+    id: 21,
+    name: "Netlify",
+    icon: "netlify",
+  },
+  {
+    id: 22,
+    name: "Vercel",
+    icon: "vercel",
+  },
+  {
+    id: 23,
+    name: "Postman",
+    icon: "postman",
+  },
+  {
+    id: 24,
+    name: "VS Code",
+    icon: "vscode",
+  },
+  {
+    id: 25,
+    name: "IntelliJ",
+    icon: "intellij",
+  },
 ];
